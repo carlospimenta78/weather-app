7d77307d4b33341d72a57b2174ffd4305c783352
Carlos Pimenta <carlospimenta@credisegur.pt>
2022-09-02 17:29:48 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
 
+import React, { useEffect, useState } from 'react';
+
 function App() {
+	const [latitude, setLatitude] = useState('');
+	const [longitude, setLongitude] = useState('');
+
+	const [data, setData] = useState();
+
+	const updateValues = e => {
+		fetch(
+			`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,windspeed_10m`
+		)
+			.then(e => e.json())
+			.then(info => setData(info));
+		e.preventDefault();
+	};
+
+	console.log(data);
 	return (
 		<div>
-			<span>test</span>
+			<form onSubmit={updateValues}>
+				<label>Latitude</label>
+				<input
+					value={latitude}
+					onChange={event => setLatitude(event.target.value)}
+				></input>
+
+				<label>Longitude</label>
+				<input
+					value={longitude}
+					onChange={event => setLongitude(event.target.value)}
+				></input>
+				<button type='submit'>Submit</button>
+			</form>
 		</div>
 	);
 }
